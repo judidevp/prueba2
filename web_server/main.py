d8fbc3ee56dada8e4e51f421ccf737dcc97789ad
diego_linux <judicoder@gmail.com>
2023-02-18 16:31:00 -0400

    creando mi primer servidor

--- web_server/main.py
+++ web_server/main.py
@@ -1,11 +1,36 @@
 import store
 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse 
+
+app =FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+'''
+@app.get('/contact')
+def get_list():
+    return {'name': 'platzi'}
+'''
+#para retornar HTML
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return"""
+    <h1>JUAN DIEGO</h1>
+    <p>este es mi primer servidor</p>
+    """
+
+
+
+
 def run():
     store.get_categories()
 
 if __name__ =='__main__':
     run()
 
-#----
+#----------
 
     
